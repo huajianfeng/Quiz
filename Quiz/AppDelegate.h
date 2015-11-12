34ddea7d386f9684204f428b5fbb13feb9c0a1cb
Netease <netease@NeteasedeMac-Pro-8.local>
2015-11-12 16:49:57 +0800

    first commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Netease on 15/9/11.
+//  Copyright (c) 2015年 Netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz
+//
+//  Created by Netease on 15/9/11.
+//  Copyright (c) 2015年 Netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
