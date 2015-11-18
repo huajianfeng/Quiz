d10188d102ec6e6b7b5b39eb32078c01d9da0098
Netease <netease@NeteasedeMac-Pro-8.local>
2015-11-18 16:57:27 +0800

    reverse third commit

--- Quiz/AppDelegate.m
+++ Quiz/AppDelegate.m
@@ -13,7 +13,7 @@
 
 @end
 
-@implementation AppDelegate3
+@implementation AppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
